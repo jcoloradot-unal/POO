b9447b8423b3b5d11cd1316bfcdaea8b282ebd04
jcoloradot-unal <jcoloradot@unal.edu.co>
2026-06-12 17:31:11 -0500

    Clase Mensaje

---
+++ mensaje.java
@@ -0,0 +1,41 @@
+class Mensaje {
+	String mensajeId;
+	String asunto;
+	String cuerpo;
+	String emisor;
+	UsuarioEmpresa receptor;
+
+	public Mensaje(String asunto, String cuerpo, String emisor, UsuarioEmpresa receptor) {
+		mensajeId = UUID.randomUUID().toString().replace("-", "").substring(0,6);
+		this.asunto = asunto;
+		this.cuerpo = cuerpo;
+		this.emisor = emisor;
+		this.receptor = receptor;
+	}
+
+	public String getMensajeId() {
+		return mensajeId;
+	}
+	public String getAsunto() {
+		return asunto;
+	}
+	public String getCuerpo() {
+		return cuerpo;
+	}
+	public String getEmisor() {
+		return emisor;
+	}
+	public String getReceptor() {
+		return receptor;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+	public void setCuerpo(String cuerpo) {
+		this.cuerpo = cuerpo;
+	}
+	public void setReceptor(UsuarioEmpresa receptor) {
+		this.receptor = receptor;
+	}
+}
