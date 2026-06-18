442c1d35d6120f9f573b9581a35e164289f1d070
Moonboy-glitch <carlostivenv@gmail.com>
2026-06-17 23:31:44 -0500

    Update EnviarMensaje.java

--- Menus/EnviarMensaje.java
+++ Menus/EnviarMensaje.java
@@ -1,8 +1,30 @@
 package Menus;
 
+import java.util.Scanner;
+
 public class EnviarMensaje extends Menu {
-   @Override
+    @Override
     public void iniciar(GestorMenus gestor) {
-        gestor.cambiarMenu("Menu Principal");
-    } 
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("\n--- ENVIAR MENSAJE ---");
+        System.out.println("Estos son tus mensajes listos para enviar:\n");
+        
+        gestor.sistema.getUsuarioActivo().imprimirBorradores();
+        
+        System.out.println("\nIngrese el ID del mensaje que desea despachar:");
+        String msgId = sc.nextLine();
+        
+        boolean exitoso = gestor.sistema.getUsuarioActivo().enviarMensaje(msgId);
+        
+        if (exitoso) {
+            System.out.println("¡El mensaje ha sido enviado correctamente!");
+        } else {
+            System.out.println("Error: No se encontró ningún borrador con ese ID.");
+        }
+        
+        System.out.println("\nIngrese cualquier letra y presione Enter para volver");
+        sc.next();
+        gestor.cambiarMenu("Mensajes");
+    }
 }
