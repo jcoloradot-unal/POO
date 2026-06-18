3f0145b2c98d8a798f81f8d8188ef92fa6b85ea3
Moonboy-glitch <carlostivenv@gmail.com>
2026-06-17 23:31:36 -0500

    Update RedactarMensaje.java

--- Menus/RedactarMensaje.java
+++ Menus/RedactarMensaje.java
@@ -1,8 +1,35 @@
 package Menus;
 
+import Mensajes.UsuarioEmpresa;
+import java.util.Scanner;
+
 public class RedactarMensaje extends Menu {
     @Override
     public void iniciar(GestorMenus gestor) {
-        gestor.cambiarMenu("Menu Principal");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("\n--- REDACTAR MENSAJE ---");
+        System.out.println("Ingrese el nombre exacto del destinatario:");
+        String nombreDest = sc.nextLine();
+        
+        UsuarioEmpresa destinatario = gestor.sistema.buscarUsuario(nombreDest);
+        
+        if (destinatario == null) {
+            System.out.println("Error: El usuario no existe en el sistema.");
+        } else {
+            System.out.println("Ingrese el asunto del mensaje:");
+            String asunto = sc.nextLine();
+            
+            System.out.println("Ingrese el cuerpo del mensaje:");
+            String cuerpo = sc.nextLine();
+            
+            // Se guarda en los borradores del usuario que tiene la sesión activa
+            gestor.sistema.getUsuarioActivo().redactarMensaje(cuerpo, asunto, destinatario);
+            System.out.println("Mensaje guardado en BORRADORES exitosamente.");
+        }
+        
+        System.out.println("\nIngrese cualquier letra y presione Enter para volver");
+        sc.next();
+        gestor.cambiarMenu("Mensajes");
     }
 }
