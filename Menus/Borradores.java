0b9592be42ebf00d7d628630c9bd56db23a7c87a
Moonboy-glitch <carlostivenv@gmail.com>
2026-06-17 23:32:04 -0500

    Update Borradores.java

--- Menus/Borradores.java
+++ Menus/Borradores.java
@@ -1,8 +1,18 @@
 package Menus;
 
+import java.util.Scanner;
+
 public class Borradores extends Menu {
     @Override
     public void iniciar(GestorMenus gestor) {
-        gestor.cambiarMenu("Menu Principal");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n--- TUS BORRADORES ---");
+        
+        gestor.sistema.getUsuarioActivo().imprimirBorradores();
+        
+        System.out.println("\nIngrese cualquier letra y presione Enter para volver");
+        sc.next();
+        
+        gestor.cambiarMenu("Mensajes");
     }
 }
